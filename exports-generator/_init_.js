f7d9d8cd088492bac60992750764b31b9a21aaf3
Craig <c.chick@tx-consulting.com>
2023-07-31 02:29:50 +0200

    CLI documentation

--- exports-generator/_init_.js
+++ exports-generator/_init_.js
@@ -1,22 +1,35 @@
 #!/usr/bin/env node
 
+/**
+ *- Init script for CLI commands.
+ *- Add the exports-generator script package to to your project root
+ *- then create package.json scripts to run exports-generator
+ *------
+ *- <-------------------------------------------------------------->
+ *- eg:
+ *- --"scripts": {
+ *- ---- "exports-gen": "node scripts/exports-generator src/pages
+ *- --}
+ *- Will generate exports file for all found export default modules at src/pages
+ *- <-------------------------------------------------------------->
+ *- Chain other paths to the command seperated by a space
+ *- ------- eg: src/pages src/lib
+ *- to generate multiple exports files, run the command again to update export files
+ */
+
 const ExportsGenerator = require('./class');
 
-// Get the paths from the command line arguments (start from index 2 to skip 'node' and 'index.js')
 const paths = process.argv.slice(2);
 
-// Check if any paths are provided as arguments
 if (paths.length === 0) {
   console.error('Error: You must provide at least one path as an argument.');
   console.log('Usage: node exports-generator <path1> <path2> ...');
-  process.exit(1); // Exit the process with an error status code
+  process.exit(1);
 }
 
-// Function to generate exports for a single path
 function generateExportsForPath(path) {
   const exportsGeneratorInstance = new ExportsGenerator(path);
   exportsGeneratorInstance.generate();
 }
 
-// Generate exports for each provided path
 paths.forEach(generateExportsForPath);
